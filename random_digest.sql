8cbec46ad0f4505c8c1a79690c0e4fc27bb49c58
Denver Timothy <denver@timothy.io>
2016-03-28 20:11:37 -0600

    new snippets

---
+++ random_digest.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE FUNCTION random_md5() RETURNS text LANGUAGE sql
+AS $$ SELECT md5(gen_random_bytes(1024)); $$;
+
+CREATE OR REPLACE FUNCTION sha256(subject bytea) RETURNS text LANGUAGE sql
+AS $function$ SELECT ENCODE(DIGEST($1, 'sha256'), 'hex'); $function$
+
+CREATE OR REPLACE FUNCTION random_sha256() RETURNS text LANGUAGE sql
+AS $$ SELECT sha256(gen_random_bytes(1024)); $$;
