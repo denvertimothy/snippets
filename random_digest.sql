a70460380975f87a5159972ecedc45e4dba0ba7e
Ian Timothy <ian@timothy.io>
2017-07-05 12:05:54 -0600

    Update random_digest.sql

--- random_digest.sql
+++ random_digest.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION random_md5() RETURNS text LANGUAGE sql
 AS $$ SELECT md5(gen_random_bytes(1024)); $$;
 
 CREATE OR REPLACE FUNCTION sha256(subject bytea) RETURNS text LANGUAGE sql
-AS $function$ SELECT ENCODE(DIGEST($1, 'sha256'), 'hex'); $function$
+AS $$ SELECT ENCODE(DIGEST($1, 'sha256'), 'hex'); $$;
 
 CREATE OR REPLACE FUNCTION random_sha256() RETURNS text LANGUAGE sql
 AS $$ SELECT sha256(gen_random_bytes(1024)); $$;
