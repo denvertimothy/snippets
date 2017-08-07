bc51d516f45ae5570bc9174c6d52fc11b4f0e498
Ian Timothy <ian@timothy.io>
2017-08-06 22:42:20 -0600

    Update encrypt_feistel_24.sql

--- encrypt_feistel_24.sql
+++ encrypt_feistel_24.sql
@@ -3,14 +3,12 @@ CREATE OR REPLACE FUNCTION encrypt_feistel_24(digits BIGINT)
 	LANGUAGE plpgsql STRICT IMMUTABLE
 	AS $$
 DECLARE
-	l1 INTEGER;
-	l2 INTEGER;
+	l1 INTEGER := (digits >> 16) & 65535;
+	l2 INTEGER := digits & 65535;
 	r1 INTEGER;
 	r2 INTEGER;
 	i INTEGER := 0;
 BEGIN
-	l1:= (digits >> 16) & 65535;
-	r1:= digits & 65535;
 	WHILE i < 3 LOOP
 		l2 := r1;
 		r2 := l1 # ((((1366 * r1 + 150889) % 714025) / 714025.0) * 32767)::INTEGER;
