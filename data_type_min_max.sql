3c4367dc982969cab9fb4fbde3ef3bd165b5ff45
Ian Timothy <ian@timothy.io>
2017-09-19 23:09:27 -0600

    Update data_type_min_max.sql

--- data_type_min_max.sql
+++ data_type_min_max.sql
@@ -1,15 +1,11 @@
 CREATE OR REPLACE FUNCTION value_bigint_max()
 RETURNS BIGINT
 LANGUAGE sql AS $$
-BEGIN
     SELECT -(((2^(8*pg_column_size(1::BIGINT)-2))::BIGINT << 1)+1);
-END;
 $$;
 
 CREATE OR REPLACE FUNCTION value_bigint_min()
 RETURNS BIGINT
 LANGUAGE sql AS $$
-BEGIN
     SELECT (2^(8*pg_column_size(1::bigint)-2))::bigint << 1;
-END;
 $$;
