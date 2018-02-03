8a8b3fbe45c16d902ac3e0798ba2bddbeac782b8
Ian Timothy <ian@timothy.io>
2018-02-02 18:21:02 -0700

    Update import_mysql.sql

--- import_mysql.sql
+++ import_mysql.sql
@@ -21,10 +21,10 @@ begin
 	execute format($$create schema if not exists %I$$, local_schema);
 	
 	-- create foreign server
-	execute format($$create server if not exists %I foreign data wrapper mysql_fdw options (host $1, port $2)$$, local_server) using foreign_host, foreign_port;
+	execute format($$create server %I foreign data wrapper mysql_fdw options (host '%s', port '%s')$$, local_server, foreign_host, foreign_port);
 	
 	-- create user mapping
-	execute format($$create user mapping if not exists for %I server %I options (username $1, password $2)$$, local_user, local_server) using foreign_username, foreign_password;
+	execute format($$create user mapping for %I server %I options (username '%s', password '%s')$$, local_user, local_server, foreign_username, foreign_password);
 	
 	-- import foreign schema
 	execute format($$import foreign schema %I from server %I into %I$$, foreign_database, local_server, local_schema);
@@ -32,17 +32,15 @@ begin
 	-- change dates/times to text
 	if(change_date_types_to_text is true) then
 		for t in
-			execute
 			select attrelid::regclass, n.nspname, c.relname, a.attname
 			from pg_attribute as a
 				join pg_class as c on (a.attrelid = c.oid)
 				join pg_namespace as n on (c.relnamespace = n.oid)
-			where n.nspname=$1
+			where n.nspname=local_schema
 				and a.attname like '%_at'
-			using local_schema;
 		loop
 			execute format('alter foreign table %I.%I alter column %I type text', t.nspname, t.relname, t.attname);
 		end loop;
 	end if;
 end;
-$$function$$;
+$function$;
