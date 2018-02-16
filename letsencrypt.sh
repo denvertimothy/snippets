ba00c14936cd22b91f8a62574e5ab871af661124
Ian Timothy <ian@timothy.io>
2018-02-15 17:00:56 -0700

    Update letsencrypt.sh

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,5 +1,5 @@
 # setup new certificate
-certbot certonly -n -q --webroot --cert-name <internal name> -w <webroot> -d <domain1> -d <domain2>
+certbot certonly -n -q --webroot --cert-name <internal name> -w <webroot> -d <domain1> -d <domain2> --agree-tos --email <email>
 
 # rewnew all certificates
 certbot rewnew -n -q
