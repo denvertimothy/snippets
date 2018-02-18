85c36be0338a9774469e886465da4f5d81b26900
Ian Timothy <ian@timothy.io>
2018-02-18 13:50:04 -0700

    Update encrypt_feistel_24.sql

--- encrypt_feistel_24.sql
+++ encrypt_feistel_24.sql
@@ -1,5 +1,5 @@
 /*
-	encrypt_feistel_24(digits)
+	encrypt_feistel(digits)
 	
 	Takes an integer as parameter and returns non-colliding pseudo-randomized integer.
 	
@@ -15,16 +15,14 @@
 	https://medium.com/@emerson_lackey/postgres-randomized-primary-keys-123cb8fcdeaf
 */
 
-CREATE OR REPLACE FUNCTION encrypt_feistel_24(digits INTEGER)
-	RETURNS INTEGER
-	LANGUAGE plpgsql STRICT IMMUTABLE
-	AS $$
+CREATE OR REPLACE FUNCTION encrypt_feistel(digits INTEGER)
+	RETURNS integer LANGUAGE plpgsql STRICT IMMUTABLE AS $$
 DECLARE
-	l1 INTEGER := (digits >> 16) & 65535;
-	l2 INTEGER;
-	r1 INTEGER := digits & 65535;
-	r2 INTEGER;
-	i INTEGER := 0;
+	l1 integer := (digits >> 16) & 65535;
+	l2 integer;
+	r1 integer := digits & 65535;
+	r2 integer;
+	i integer := 0;
 BEGIN
 	WHILE i < 3 LOOP
 		l2 := r1;
@@ -35,3 +33,25 @@ BEGIN
 	END LOOP;
 	RETURN ((r1 << 16) + l1);
 END;$$;
+
+CREATE OR REPLACE FUNCTION encrypt_feistel(VALUE bigint)
+	RETURNS bigint LANGUAGE plpgsql STRICT IMMUTABLE AS $$
+DECLARE
+	l1 bigint;
+	l2 bigint;
+	r1 bigint;
+	r2 bigint;
+	i integer := 0;
+BEGIN
+    l1:= (VALUE >> 32) & 4294967295::bigint;
+    r1:= VALUE & 4294967295;
+    WHILE i < 3 LOOP
+        l2 := r1;
+        r2 := l1 # ((((1366.0 * r1 + 150889) % 714025) / 714025.0) * 32767*32767)::int;
+        l1 := l2;
+        r1 := r2;
+    i := i + 1;
+    END LOOP;
+RETURN ((r1::bigint << 32) + l1);
+END;
+$$ LANGUAGE plpgsql strict immutable;
