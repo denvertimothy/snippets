5694d1435645cce82baf5bf81e80f5d4f7931131
Ian Timothy <ian@timothy.io>
2018-02-18 17:41:16 -0700

    Create random_string.sql

---
+++ random_string.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE FUNCTION public.random_string(len integer DEFAULT 16, chars text DEFAULT 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&#%@!_-^*()@+=?/<>%[]{}'::text)
+ RETURNS text
+ LANGUAGE plpgsql
+ STRICT
+AS $function$
+BEGIN
+	RETURN string_agg(substr(chars, ceil(random() * length(chars))::integer, 1), '') from generate_series(1, len);
+END;$function$;
