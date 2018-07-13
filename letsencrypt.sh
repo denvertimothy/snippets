0dc1e1b48da11f51bf399074b415e000fffb64a0
Ian Timothy <ian@timothy.io>
2018-07-12 22:58:48 -0600

    Update letsencrypt.sh

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,5 +1,8 @@
-# setup new certificate
+# setup new certificate using webroot authentication
 certbot certonly -n -q --webroot --cert-name <internal name> -w <webroot> -d <domain1> -d <domain2> --agree-tos --email <email>
+
+# setup new certificate using standalone authentication
+certbot certonly -n -q --standalone --tls-sni-01-port 4443 --http-01-port 8880 --cert-name <internal name> -d <domain1> -d <domain2> --agree-tos --email ian@thrivedata.it
 
 # rewnew all certificates
 certbot rewnew -n -q
