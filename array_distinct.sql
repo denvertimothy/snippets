8057d7f3d59df3954e8c12c694c5a3fd25a50ed8
Ian Timothy <ian@timothy.io>
2018-08-01 20:44:18 -0600

    Create array_distinct.sql

---
+++ array_distinct.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE FUNCTION array_distinct(anyarray)
+RETURNS anyarray LANGUAGE sql IMMUTABLE
+AS $function$
+	SELECT array_agg(DISTINCT x ORDER BY x) FROM unnest($1) AS x ORDER BY 1;
+$function$;
