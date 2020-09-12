6b120c83edac9eedefbf45c62d470521283fb555
Ian Timothy <ian@timothy.io>
2020-09-12 03:32:55 -0600

    add argument 'drop_existing' to first drop trigger if it already exists before installing

--- postgresql/row_updated_when.sql
+++ postgresql/row_updated_when.sql
@@ -6,16 +6,21 @@ BEGIN
 	IF (NEW IS DISTINCT FROM OLD) THEN
 		NEW.updated_when = NOW();
 	END IF;
+	
 	RETURN NEW;
 END;
 $$;
 
-CREATE OR REPLACE PROCEDURE install_trigger_updated_when(target_table regclass)
+CREATE OR REPLACE PROCEDURE install_trigger_updated_when(target_table regclass, drop_existing boolean)
 LANGUAGE plpgsql AS $$
 BEGIN
+	IF (drop_existing) THEN
+		EXECUTE format('DROP TRIGGER updated_when ON %I', target_table);
+	END IF;
+
 	EXECUTE format(
 		'CREATE TRIGGER updated_when '
-			'BEFORE UPDATE ON %s '
+			'BEFORE UPDATE ON %I '
 			'FOR EACH ROW '
 				'EXECUTE PROCEDURE updated_when()',
 		target_table
