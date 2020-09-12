ce4326b0251f88cf6003f21297ab9579b1947be0
Ian Timothy <ian@timothy.io>
2020-09-12 03:15:34 -0600

    move new vs old condition out of trigger and into function to deal with generated columns

--- postgresql/row_updated_when.sql
+++ postgresql/row_updated_when.sql
@@ -1,14 +1,23 @@
-CREATE FUNCTION updated_when()
-RETURNS trigger LANGUAGE plpgsql AS $function$
+CREATE OR REPLACE FUNCTION updated_when()
+RETURNS trigger LANGUAGE plpgsql AS $$
 BEGIN
-	NEW.updated_when = NOW();
+	-- the condition is here in the function instead of in the trigger
+	-- in order to deal with generated columns
+	IF (NEW IS DISTINCT FROM OLD) THEN
+		NEW.updated_when = NOW();
+	END IF;
 	RETURN NEW;
 END;
-$function$;
+$$;
 
-CREATE FUNCTION install_trigger_updated_when(target_table regclass)
-RETURNS void LANGUAGE plpgsql AS $function$
+CREATE OR REPLACE PROCEDURE install_trigger_updated_when(target_table regclass)
+LANGUAGE plpgsql AS $$
 BEGIN
-	EXECUTE format('CREATE TRIGGER updated_when BEFORE UPDATE ON %s FOR EACH ROW WHEN (NEW.* IS DISTINCT FROM OLD.*) EXECUTE PROCEDURE updated_when()', target_table);
-END;
-$function$;
+	EXECUTE format(
+		'CREATE TRIGGER updated_when '
+			'BEFORE UPDATE ON %s '
+			'FOR EACH ROW '
+				'EXECUTE PROCEDURE updated_when()',
+		target_table
+	);
+END;$$;
